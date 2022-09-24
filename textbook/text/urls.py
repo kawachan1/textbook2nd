24d20401928c3a7f6bb20256fa6036f489292abd
Syutaro Kawahata <s1f102002200@iniad.org>
2022-09-24 18:31:56 +0900

    概要

--- textbook/text/urls.py
+++ textbook/text/urls.py
@@ -2,6 +2,10 @@ from importlib.resources import path
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.index, name="index"),
-}
+    path('s1', views.s1, name='s1'),
+    path('s2', views.s2, name='s2'),
+    path('s3', views.s3, name='s3'),
+
+]
--- textbook/text/views.py
+++ textbook/text/views.py
@@ -3,5 +3,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-    return render(request,'text/index.html')
+    return render(request, 'text/index.html')
+
+
+def s1(request):
+    return render(request, 'text/s1.html')
+
+
+def s2(request):
+    return render(request, 'text/s2.html')
+
+
+def s3(request):
+    return render(request, 'text/s3.html')
